e16cbd61519d51f37a126e0fb9dd69210e6f6d57
sahinkarahan <sahin.karahan@hotmail.com>
2025-01-17 22:05:58 +0300

    Initial Commit

---
+++ FlashCard-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlashCard-1
+//
+//  Created by Şahin Karahan on 17.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlashCard-1/FlashCard_1App.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCard_1App.swift
+//  FlashCard-1
+//
+//  Created by Şahin Karahan on 17.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlashCard_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
